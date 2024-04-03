c5e079e3dcf60ba4353c499234f5f3a94262b394
ppavlov <ppavlov.frontend@gmail.com>
2024-04-03 22:45:53 +0200

    fix_style_dropbox_menu

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -4,12 +4,15 @@ import { colors } from 'styles/colors'
 
 export const FooterMain = styled.footer`
   width: 100%;  
-  min-height: 225px;
   background-color: ${colors.baseGrey80};
   color: ${colors.white};
 `;
 export const FooterMainContainer = styled.footer`
-  width: 100%;  
+  width: 100%;
+  min-height: 200px;
   max-width: 1400px;
   margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
