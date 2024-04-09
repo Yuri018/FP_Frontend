a16e19dec78e000c9e5713da9a23a53b4ffc941f
ppavlov <ppavlov.frontend@gmail.com>
2024-04-09 17:32:20 +0200

    add_newsCard

--- src/components/MainLowerPart/MainLowerPart.tsx
+++ src/components/MainLowerPart/MainLowerPart.tsx
@@ -1,23 +1,49 @@
+import NewsList from "components/NewsList/NewsList";
 import {
   MainLowerPartContainer,
   MainLowerPartFlex,
-  MainLowerPartItem,
-  MainLowerPartTitle,
 } from "./styles";
+
+const newsData = [
+  { 
+    imgUrl: "https://www.berlin.de/binaries/asset/image_assets/8239409/ratio_4_3/1685087592/800x600/", 
+    title: 'Mobilität und Verkehr in Berlin', 
+    content: 'Miteinander statt gegeneinander: In Berlin nehmen alle Verkehrsteilnehmende gleichberechtigt am Straßenverkehr teil.' 
+  },
+  { 
+    imgUrl: "https://www.berlin.de/binaries/asset/image_assets/8237399/ratio_4_3/1685010433/800x600/", 
+    title: 'Moderne Verwaltung in Berlin ', 
+    content: 'Berlin wird digitalisiert und modernisiert. Für eine Stadt, die auf allen Ebenen funktioniert.' 
+  },
+  {
+    imgUrl: "https://www.berlin.de/binaries/asset/image_assets/8237900/ratio_4_3/1685015621/800x600/", 
+    title: 'Bauen und Wohnen in Berlin', 
+    content: 'Der Senat setzt auf schnellen Neubau, bezahlbare Wohnungen und wirksamen Mieterschutz.' 
+  },
+  {
+
+    imgUrl: "https://www.berlin.de/binaries/asset/image_assets/8239409/ratio_4_3/1685087592/800x600/", 
+    title: 'Mobilität und Verkehr in Berlin', 
+    content: 'Miteinander statt gegeneinander: In Berlin nehmen alle Verkehrsteilnehmende gleichberechtigt am Straßenverkehr teil.' 
+  },
+  { 
+    imgUrl: "https://www.berlin.de/binaries/asset/image_assets/8237399/ratio_4_3/1685010433/800x600/", 
+    title: 'Moderne Verwaltung in Berlin ', 
+    content: 'Berlin wird digitalisiert und modernisiert. Für eine Stadt, die auf allen Ebenen funktioniert.' 
+  },
+  {
+    imgUrl: "https://www.berlin.de/binaries/asset/image_assets/8237900/ratio_4_3/1685015621/800x600/", 
+    title: 'Bauen und Wohnen in Berlin', 
+    content: 'Der Senat setzt auf schnellen Neubau, bezahlbare Wohnungen und wirksamen Mieterschutz.' 
+  },
+
+];
 
 function MainLowerPart() {
   return (
     <MainLowerPartContainer>
       <MainLowerPartFlex>
-        <MainLowerPartItem>
-          <MainLowerPartTitle>Main Lower Part Item</MainLowerPartTitle>
-        </MainLowerPartItem>
-        <MainLowerPartItem>
-          <MainLowerPartTitle>Main Lower Part Item</MainLowerPartTitle>
-        </MainLowerPartItem>
-        <MainLowerPartItem>
-          <MainLowerPartTitle>Main Lower Part Item</MainLowerPartTitle>
-        </MainLowerPartItem>
+          <NewsList newsList={newsData} />
       </MainLowerPartFlex>
     </MainLowerPartContainer>
   )
---
+++ src/components/NewsCard/NewsCard.tsx
@@ -0,0 +1,43 @@
+import { CardActionArea, CardContent, Typography, CardActions } from '@mui/material';
+import { MyButton, MyCardMedia, MyCard } from './styles';
+
+interface News {
+  imgUrl: string;
+  title: string;
+  content: string;
+}
+
+interface NewsCardProps {
+  news: News;
+}
+
+const NewsCard: React.FC<NewsCardProps> = ({ news }) => {
+  return (
+    <>
+    <MyCard>
+      <CardActionArea>
+        <MyCardMedia
+          image={news.imgUrl}
+          title="test"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+          {news.title} 
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          {news.content}
+            
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions sx={{padding: 2}}>
+        <MyButton size="small" variant="contained" color="primary" >
+          Learn More
+        </MyButton>
+      </CardActions>
+  </MyCard>
+    </>
+  );
+};
+
+export default NewsCard;
