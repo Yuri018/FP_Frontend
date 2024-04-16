ecff08ef1769cc4016d06a9149719eef5909827e
AGaleea <a_galeeva@hotmail.com>
2024-04-16 16:22:47 +0200

    part of InfoCards added

--- src/components/MainLowerPart/MainLowerPart.tsx
+++ src/components/MainLowerPart/MainLowerPart.tsx
@@ -1,26 +1,23 @@
-import { useState, useEffect } from "react";
-// import { useLocation, useNavigate } from "react-router-dom";
-import InfoList from "components/InfoList/InfoList";
-import NewsList from "components/NewsList/NewsList";
-import { instance } from "../../utils/axios";
-import {
-  MainLowerPartContainer,
-  MainLowerPartFlex,
-} from "./styles";
-import { title } from "process";
+import { useState, useEffect } from "react"
+import { useLocation, useNavigate } from "react-router-dom"
+import InfoList from "components/InfoList/InfoList"
+import NewsList from "components/NewsList/NewsList"
+import { instance } from "../../utils/axios"
+import { MainLowerPartContainer, MainLowerPartFlex } from "./styles"
+import { title } from "process"
 
 interface NewsProps {
   title: string
   shortDescription: string
   imgUrl: string
-  content: string  
+  content: string
 }
 interface InfoProps {
   link: string
   title: string
   description: string
   address: string
-  tel: string 
+  tel: string
 }
 
 export interface NewsListProps {
@@ -31,6 +28,7 @@ export interface InfoListProps {
 }
 
 function MainLowerPart() {
+  const location = useLocation()
   const [newsData, setNewsData] = useState<NewsProps[]>([])
   const [infoData, setinfoData] = useState<InfoProps[]>([])
 
@@ -55,32 +53,54 @@ function MainLowerPart() {
     fetchNewsData()
   }, [location.pathname])
 
+  useEffect(() => {
+    async function fetchInfoData() {
+      try {
+        let endpoint = "/berlin"
+        if (location.pathname === "/berlin/cafes-restaurants") {
+          endpoint = "/berlin/restaurants_info"
+        } else if (location.pathname === "/berlin/children") {
+          endpoint = "/berlin/children_info"
+        } else if (location.pathname === "/other_path3") {
+          endpoint = "/other_endpoint3"
+        }
+
+        const response = await instance.get<InfoProps[]>(endpoint)
+        setinfoData(response.data)
+      } catch (error) {
+        console.error("Error fetching news data:", error)
+      }
+    }
+    fetchInfoData()
+  }, [location.pathname])
+
   return (
     <MainLowerPartContainer>
       <MainLowerPartFlex>
-      {location.pathname === "/" && (
-        newsData.length > 0 ? (
-          <NewsList newsList={newsData} />
-        ) : (
-          <div>Loading...</div>
-        )
-      ) }
-      {location.pathname === "/general-info" && (
-        newsData.length > 0 ? (
-          <NewsList newsList={newsData} />
-        ) : (
-          <div>Loading...</div>
-        )
-      ) }
-
-      {location.pathname === "/berlin" && (
-        infoData.length > 0 ? (
-          <InfoList infoList={infoData}/>
-        ) : (
-          <div>Loading...</div>
-        )
-      )}
-      
+        {location.pathname === "/" &&
+          (newsData.length > 0 ? (
+            <NewsList newsList={newsData} />
+          ) : (
+            <div>Loading...</div>
+          ))}
+        {location.pathname === "/general-info" &&
+          (newsData.length > 0 ? (
+            <NewsList newsList={newsData} />
+          ) : (
+            <div>Loading...</div>
+          ))}
+        {location.pathname === "/berlin/cafes-restaurants" &&
+          (infoData.length > 0 ? (
+            <InfoList infoList={infoData} />
+          ) : (
+            <div>Loading...</div>
+          ))}
+        {location.pathname === "/berlin/children" &&
+          (infoData.length > 0 ? (
+            <InfoList infoList={infoData} />
+          ) : (
+            <div>Loading...</div>
+          ))}
       </MainLowerPartFlex>
     </MainLowerPartContainer>
   )
