d7394e9ba5f9ac7c8747f6d156becd5af5ff569d
AGaleea <a_galeeva@hotmail.com>
2024-04-17 14:16:23 +0200

    doctors dropdown menu's closure fixed

--- src/pages/Berlin/Berlin.tsx
+++ src/pages/Berlin/Berlin.tsx
@@ -2,19 +2,11 @@ import MainUpperPart from "components/MainUpperPart"
 import Header from "components/Header"
 import MainLowerPart from "components/MainLowerPart"
 
-import {
-  BerlinImg,
-  BerlinLogo,
-  Guitars,
-  Butterfly,
-  TwoPeople,
-  University,
-} from "assets"
+import { BerlinImg, BerlinLogo } from "assets"
 
 import Footer from "components/Footer"
 
-function Berlin_Restaurants() {
-
+function Berlin() {
   const cityNavLinks = [
     { to: "/berlin/doctors", text: "Врачи" },
     { to: "/berlin/children", text: "Дети" },
@@ -35,14 +27,6 @@ function Berlin_Restaurants() {
       <MainUpperPart
         bcgImgDescr={{ src: BerlinImg, alt: "BerlinImg" }}
         mainTitle="Новости Берлина и Бранденбурга"
-        // firstImgDescr={{ src: Guitars, alt: "Guitars" }}
-        // secondImgDescr={{ src: Butterfly, alt: "Butterfly" }}
-        // thirdImgDescr={{ src: TwoPeople, alt: "Two people" }}
-        // fourthImgDescr={{ src: University, alt: "University" }}
-        // firstImgTitle="Музыкальные истории"
-        // secondImgTitle="Загадки природы"
-        // thirdImgTitle="Жизнь рядом с нами"
-        // fourthImgTitle="Высшее образование в Германии"
         buttonProps={{
           name: "Читать подробнее",
           onClick: () => {
@@ -56,4 +40,4 @@ function Berlin_Restaurants() {
   )
 }
 
-export default Berlin_Restaurants
+export default Berlin
