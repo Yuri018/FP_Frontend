8822ec532ed4e098faf51f96420a2b181bf43528
AGaleea <a_galeeva@hotmail.com>
2024-04-18 13:21:38 +0200

    footer added

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,17 +1,53 @@
 import {
   FooterMain,
-  FooterMainContainer
-} from "./styles";
+  FooterMainContainer,
+  FooterRow,
+  FooterContainerOne,
+  FooterContainerTwo,
+  FooterContainerThree,
+  ContactInfo,
+  TermsLinks,
+  SocialIcons,
+  Separator,
+} from "./styles"
+import { FaFacebook, FaYoutube, FaInstagram } from "react-icons/fa"
 
 function Footer() {
   return (
     <FooterMain>
       <FooterMainContainer>
-        FOOTER
+        <FooterRow>
+          <FooterContainerOne>
+            <img src="/src/assets/GermanyMain2.png" alt="GermanMainLogo" />
+          </FooterContainerOne>
 
+          <FooterContainerTwo>
+            <TermsLinks href="/src/assets/TermsOfUse.html" target="_blank">
+              Условия использования
+            </TermsLinks>
+          </FooterContainerTwo>
+
+          <FooterContainerThree>
+            <SocialIcons href="#">
+              <FaFacebook />
+            </SocialIcons>
+            <SocialIcons href="#">
+              <FaYoutube />
+            </SocialIcons>
+            <SocialIcons href="#">
+              <FaInstagram />
+            </SocialIcons>
+          </FooterContainerThree>
+        </FooterRow>
+
+        <Separator />
+
+        <ContactInfo>
+          Германия, Берлин, Бранденбургские ворота, Центральный вход © Team 24
+        </ContactInfo>
       </FooterMainContainer>
     </FooterMain>
   )
 }
 
-export default Footer;
+export default Footer
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,19 +1,80 @@
-import styled from "styled-components";
-
-import { colors } from 'styles/colors'
+import styled from "styled-components"
+import { FaFacebook, FaYoutube, FaInstagram } from "react-icons/fa"
 
 export const FooterMain = styled.footer`
-  width: 100%;  
-  background-color: ${colors.baseGrey80};
-  color: ${colors.white};
-  margin-top: auto;
-`;
-export const FooterMainContainer = styled.footer`
   width: 100%;
-  min-height: 200px;
-  max-width: 1400px;
+  background-color: #30363d;
+  padding: 20px 0;
+  color: #ffffff;
+  max-width: 1370px;
   margin: 0 auto;
+  padding: 20px;
+`
+
+export const FooterMainContainer = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
-`;
+  padding: 0 15px;
+`
+
+export const FooterRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`
+
+export const FooterContainerOne = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`
+
+export const ContactInfo = styled.p`
+  text-align: center;
+`
+
+export const FooterContainerTwo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+export const TermsLinks = styled.a`
+  color: #ffffff;
+  transition: color 0.3s ease;
+
+  &:hover {
+    color: #bbbbbb;
+  }
+`
+
+export const FooterContainerThree = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const Separator = styled.hr`
+  width: 100%;
+  border: 1px solid #ffffff;
+  margin: 20px 0;
+`
+
+export const SocialIcons = styled.a`
+  color: #ffffff;
+  margin: 0 10px;
+  transition: color 0.3s ease;
+  font-size: 34px;
+
+  &:hover {
+    color: #bbbbbb;
+  }
+`
+
+export const FacebookIcon = styled(FaFacebook)``
+
+export const YoutubeIcon = styled(FaYoutube)``
+
+export const InstagramIcon = styled(FaInstagram)``
