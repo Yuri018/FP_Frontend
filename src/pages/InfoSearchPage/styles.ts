32be09cbb7a03b023de6f96f2571e901684670da
Pavlov Pavel <pavlovpavelv1018@gmail.com>
2024-04-18 10:24:49 +0200

    fix_search_card

--- src/pages/InfoSearchPage/styles.ts
+++ src/pages/InfoSearchPage/styles.ts
@@ -1,71 +1,27 @@
 import styled from "styled-components";
+import { Grid, Card } from '@mui/material';
 
-import { colors } from 'styles/colors';
-import Button from "@mui/material/Button";
-import CardMedia from "@mui/material/CardMedia";
-import { CardActions, CardContent } from '@mui/material';
-import Card from "@mui/material/Card";
-
-export const FrontalPageWrapper = styled.div`
-  position: relative;
-
-`;
-
-export const MyButton = styled(Button)`
-    && {
-        border-radius: 50em;
-        text-transform: none;
-    }
-`;
-
-export const MyCardMedia = styled(CardMedia)`
-    && {
-        min-height: 140px;
-    }
-`;
-export const MyCard = styled(Card)`
-    && {
-        border-radius: 20px;
-        height: 100%;
-        display: flex;
-        align-items: stretch;
-        justify-content: flex-start;
-        flex-direction: column;
-    }
-`;
-export const MyCardActions = styled(CardActions)`
-    && {
-        margin-top: auto;
-    }
-`;
-export const MyCardContent = styled(CardContent)`
-    && {
-        cursor: auto;
-    }
-`;
-
-export const MainLowerPartContainer = styled.section`
+export const MainLowerPartContainer = styled.div`
   width: 100%;
   max-width: 1400px;
+  margin: 0 auto;
   padding: 0 15px;
-  margin: 0 auto 20px;
-`;
 
+`;
 export const MainLowerPartFlex = styled.div`
   display: flex;
+  flex-wrap: wrap;
   align-items: stretch;
   justify-content: flex-start;
-  flex-wrap: wrap;
 `;
 
-export const MainLowerPartItem = styled.div`
-  width: 25%;
-  padding: 20px 10px;
-  min-height: 100%;
-`;
+export const MainLowerPartItem = styled.div({
+  width: 'calc(25% - 20px)', // Adjusting for margin
+  margin: '10px',
+});
 
-export const MainLowerPartTitle = styled.div`
-  font-size: 24px;
-  font-weight: 600;
-  text-align: center
-`;
+export const InfoCard = styled(Card)({
+  display: 'flex',
+  flexDirection: 'column',
+  height: '100%',
+});
